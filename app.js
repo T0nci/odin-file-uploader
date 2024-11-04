36fb35991476e091dea2b83b59d5ad3ca3a7481c
T0nci <138928016+T0nci@users.noreply.github.com>
2024-11-04 15:09:16 +0100

    Add Express Session with a database store

--- app.js
+++ app.js
@@ -1,16 +1,39 @@
 require("dotenv");
 const express = require("express");
 const path = require("node:path");
+const session = require("express-session");
+const { PrismaSessionStore } = require("@quixo3/prisma-session-store");
+const { PrismaClient } = require("@prisma/client");
 const CustomError = require("./utils/CustomError");
 
 const app = express();
 
+// STYLE
 app.use(express.static(path.join(__dirname, "public")));
 
+// EJS
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+// FORM BODY PARSING
 app.use(express.urlencoded({ extended: true }));
+
+// EXPRESS SESSION
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: new PrismaSessionStore(new PrismaClient(), {
+      checkPeriod: 2 * 60 * 1000,
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+    cookie: {
+      maxAge: 1 * 24 * 60 * 60 * 1000,
+    },
+  }),
+);
 
 app.get("/", () => {
   throw new Error("Internal server error working!");
