d66860918a6881d5158670549440024c5c8ca1c0
T0nci <138928016+T0nci@users.noreply.github.com>
2024-11-14 21:07:29 +0100

    Add functionality for viewing shared folder files

--- controllers/shareController.js
+++ controllers/shareController.js
@@ -1,23 +1,60 @@
 const prisma = require("../prisma/client");
 const asyncHandler = require("express-async-handler");
+const { validationResult, param } = require("express-validator");
 const middleware = require("../utils/middleware");
 const CustomError = require("../utils/CustomError");
+const links = require("../utils/links");
 
-const shareRedirect = [
-  asyncHandler(middleware.cleanUpSharedFolders),
-  asyncHandler(async (req, res) => {
-    const folderInfo = await prisma.sharedFolder.findUnique({
+const validateFolderShared = () =>
+  param("folderId").custom(async (folderId) => {
+    const folder = await prisma.sharedFolder.findFirst({
       where: {
-        uuid: req.params.uuid,
+        folder_id: Number(folderId),
       },
     });
 
-    if (!folderInfo) throw new CustomError(404, "Folder Not Found.");
+    if (!folder) throw false;
+  });
 
-    res.redirect(`/share/folder/${folderInfo.folder_id}`);
+const shareRedirect = asyncHandler(async (req, res) => {
+  const folderInfo = await prisma.sharedFolder.findUnique({
+    where: {
+      uuid: req.params.uuid,
+    },
+  });
+
+  if (!folderInfo) throw new CustomError(404, "Folder Not Found.");
+
+  res.redirect(`/share/folder/${folderInfo.folder_id}`);
+});
+
+const folderGet = [
+  asyncHandler(middleware.cleanUpSharedFolders),
+  validateFolderShared(),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) throw new CustomError(404, "Folder Not Found.");
+
+    next();
+  }),
+  asyncHandler(async (req, res) => {
+    const folder = await prisma.folder.findUnique({
+      where: {
+        id: Number(req.params.folderId),
+      },
+    });
+
+    const files = await prisma.file.findMany({
+      where: {
+        folder_id: folder.id,
+      },
+    });
+
+    res.render("folderMini", { links, folder, files, currentUser: req.user });
   }),
 ];
 
 module.exports = {
   shareRedirect,
+  folderGet,
 };
--- routes/shareRouter.js
+++ routes/shareRouter.js
@@ -5,8 +5,8 @@ const shareController = require("../controllers/shareController");
 const shareRouter = Router();
 
 shareRouter.get("/:uuid", shareController.shareRedirect);
-// shareRouter.get("/folder/id", shareController.folderGet);
-// shareRouter.get("/file/id, shareController.fileGet");
-// shareRouter.get("/file/download/id", shareController.fileDownload);
+shareRouter.get("/folder/:folderId", shareController.folderGet);
+// shareRouter.get("/file/:fileId, shareController.fileGet");
+// shareRouter.get("/file/download/:fileId", shareController.fileDownload);
 
 module.exports = shareRouter;
