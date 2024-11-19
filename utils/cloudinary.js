63a2133f447181c586ced056a9998f442f1af30d
T0nci <138928016+T0nci@users.noreply.github.com>
2024-11-19 15:38:09 +0100

    Redirect uploads to a folder instead of root

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -4,6 +4,7 @@ cloudinary.config();
 module.exports = async function handleUpload(file) {
   const res = await cloudinary.uploader.upload(file, {
     resource_type: "auto",
+    asset_folder: "file_uploader",
   });
   return res;
 };
